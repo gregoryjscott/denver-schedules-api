85e0da7d5012c96a2734aeef353b435bdb590a34
Greg Scott <i@gregoryjscott.com>
2014-05-19 11:31:54 -0600

    WIP swagger works with just title

--- Schedules.API/Modules/DocsModules.cs
+++ Schedules.API/Modules/DocsModules.cs
@@ -1,5 +1,7 @@
 ﻿using Nancy;
+using Nancy.Responses;
 using Centroid;
+using System.IO;
 
 public class DocsModule : NancyModule
 {
@@ -7,14 +9,51 @@ public class DocsModule : NancyModule
     {
         StaticConfiguration.DisableErrorTraces = false;
 
+        Get ["/"] = _ => View["index.html"];
+
+        Get["/lib/{all*}"] = parameters => {
+            var fileName = (string)parameters.all;
+            return new GenericFileResponse(Path.Combine("Docs", "lib", fileName));
+        };
+
+        Get["/css/{all*}"] = parameters => {
+            var fileName = (string)parameters.all;
+            return new GenericFileResponse(Path.Combine("Docs", "css", fileName));
+        };
+
+        Get["/images/{all*}"] = parameters => {
+            var fileName = (string)parameters.all;
+            return new GenericFileResponse(Path.Combine("Docs", "images", fileName));
+        };
+
+        Get["/js/{all*}"] = parameters => {
+            var fileName = (string)parameters.all;
+            return new GenericFileResponse(Path.Combine("Docs", "js", fileName));
+        };
+
         Get ["/api-docs"] = _ => {
-            var resourceListing = Config.FromFile("Docs/resource-listing.json");
-            return Response.AsJson(resourceListing);
+            dynamic resourceListing = Config.FromFile("Docs/resource-listing.json");
+            return Response.AsJson(new {
+                apiVersion = resourceListing.apiVersion,
+                swaggerVersion = resourceListing.swaggerVersion,
+                apis = resourceListing.apis,
+                info = resourceListing.info
+            });
         };
 
         Get ["/api-docs/schedules"] = _ => {
-            var schedules = Config.FromFile("Docs/schedules.json");
-            return Response.AsJson(schedules);
+            dynamic schedules = Config.FromFile("Docs/schedules.json");
+            var response = new {
+                apiVersion = schedules.apiVersion,
+                swaggerVersion = schedules.swaggerVersion,
+                basePath = schedules.basePath,
+                resourcePath = schedules.resourcePath,
+                produces = schedules.produces,
+                authorizations = schedules.authorizations,
+                apis = schedules.apis,
+                models = schedules.models
+            };
+            return Response.AsJson(response);
         };
     }
 }
