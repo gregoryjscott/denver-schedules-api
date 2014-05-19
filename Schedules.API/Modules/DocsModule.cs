775611e6857d9999cc3c54ff63506f9155e53277
Greg Scott <i@gregoryjscott.com>
2014-05-19 16:25:48 -0600

    WIP Thanks for nothing Centroid

---
+++ Schedules.API/Modules/DocsModule.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+using System.IO;
+
+public class DocsModule : NancyModule
+{
+    public DocsModule()
+    {
+        Get["/api-docs"] = _ => FromJsonFile("resource-listing.json");
+        Get["/api-docs/schedules"] = _ => FromJsonFile("schedules.json");
+    }
+
+    static Response FromJsonFile(string jsonFile)
+    {
+        var json = File.ReadAllText(Path.Combine("Docs", jsonFile));
+        var response = (Response)json;
+        response.ContentType = "application/json";
+        return response.WithHeader("Access-Control-Allow-Origin", "*");
+    }
+}
