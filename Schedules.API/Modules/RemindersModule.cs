2411639677341443afdc4cf4a399c528fea9950c
Greg Scott <i@gregoryjscott.com>
2014-06-03 23:34:20 -0600

    WIP Use reminders.json to setup module

--- Schedules.API/Modules/RemindersModule.cs
+++ Schedules.API/Modules/RemindersModule.cs
@@ -1,15 +1,32 @@
 ﻿using Nancy;
 using Nancy.ModelBinding;
 using Schedules.API.Models;
-using System;
+using Centroid;
+using System.Linq;
 
 public class RemindersModule : NancyModule
 {
     public RemindersModule()
     {
-        Post ["/reminders"] = _ => {
-            Reminder reminder = this.Bind<Reminder>();
-            return Response.AsJson(reminder, HttpStatusCode.Created);
-        };
+        dynamic reminders = Config.FromFile("Docs/reminders.json");
+        foreach (dynamic api in reminders.apis)
+        {
+            foreach (dynamic operation in api.operations)
+            {
+                if (operation.method == "POST")
+                {
+                    var path = (string)api.path;
+                    Post[path] = _ => {
+                        Reminder reminder = this.Bind<Reminder>();
+                        return Response.AsJson(reminder, HttpStatusCode.Created);
+                    };
+                }
+            }
+        }
+
+//        Post ["/reminders"] = _ => {
+//            Reminder reminder = this.Bind<Reminder>();
+//            return Response.AsJson(reminder, HttpStatusCode.Created);
+//        };
     }
 }
